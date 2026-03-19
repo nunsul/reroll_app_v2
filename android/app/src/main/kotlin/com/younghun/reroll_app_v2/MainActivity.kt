60a53e6263b1e6f57669adfa42325300c98650d3
nunsul <younghunk17@naver.com>
2026-03-19 18:24:45 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/younghun/reroll_app_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.younghun.reroll_app_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/younghunk/reroll_app_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.younghunk.reroll_app_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
